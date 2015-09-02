48d160b67b355c58077b20f3a3b7170c0f5fbdac
Denilson Sá <denilsonsa@gmail.com>
2015-09-02 18:03:49 -0300

    Another major rewrite/refactoring of the nip2 script.
    Reading the list of images and coordinates from an external file.
    
    Other stuff!
    
    Working around some of nip2 limitations!
    
    https://github.com/jcupitt/nip2/issues/54

--- join_tiles_nip2.sh
+++ join_tiles_nip2.sh
@@ -1,153 +1,312 @@
 #!/bin/bash
+#
+# This script joins several small images into a large image.
+#
+# This script uses "nip2" tool, which uses "libvips" library.
+#
+# "libvips" is an image processing library with low memory needs.
+# It can handle 20000x20000 (or larger) images without using too much RAM.
+#
+# "nip2" is a GUI that includes its own Haskell-like scripting language. It is
+# a great way to efficiently work with several images, or large images.
+#
+# nip2 scripting language follows the functional paradigm. Here is a quick
+# summary for people who are not familiar with this language:
+#  * Statements can be defined in any order.
+#  * Evaluation is lazy.
+#  * Function call does not require parenthesis.
+#  * Variables/values are immutable.
+#  * Computation is done by returning modified versions of the input data.
+#  * Strings are lists of characters.
+#  * Lists can be indexed using the question mark.
+#      foo = [0, 1, 2];
+#      // foo?0 is the first element.
+#  * As in Lisp and Haskell, the head/tail of the list is widely used:
+#      foo = [0, 1, 2];
+#      h : t = foo;
+#      // h is 0        i.e. the first element
+#      // t is [1, 2]   i.e. a list with the remaining elements
+#  * Functions can be defined as:
+#      foo_function
+#        = value_if_first_condition, first_condition
+#        = value_if_second_condition, second_condition
+#        = default_value
+#      {
+#        statement;
+#        statement;
+#      }
+#  * List comprehensions can be defined as:
+#      foo_new_list = [ element_expression :: input_element <- input_list ];
+#      // Or using | instead of "", depending on the version.
+#      foo_new_list = [ element_expression | input_element <- input_list ];
+#
+# Due to nip2 limitations, I decided to write the file list to an external
+# instead of writing it inline in the script itself. See:
+# https://github.com/jcupitt/nip2/issues/54
 
 tilename() {
 	col="$1"
 	row="$2"
-	# Customize regarding your need.
-	echo "../../ets2/tiles/dark_google/6/${row}/${col}.png"
+
+	# Customize based on your need.
+	echo "../../ets2/tiles/dark_google/7/${row}/${col}.png"
 }
 
-echo_run() {
-	echo -E "$@"
-	"$@"
+generate_file_list() {
+	local row col first_tile next_tile tab
+	tab=$'\t'
+
+	for (( row = 0 ; row < 9999 ; ++row )) ; do
+		first_tile="$(tilename 0 ${row})"
+		[ -f "${first_tile}" ] || break
+
+		for (( col = 0 ; col < 9999 ; ++col )) ; do
+			next_tile="$(tilename ${col} ${row})"
+			[ -f "${next_tile}" ] || break
+
+			# x TAB y TAB filename NEWLINE
+			echo -E "$(( col * 256 ))${tab}$(( row * 256 ))${tab}${next_tile}"
+		done
+	done
 }
 
 nip2_script() {
-	# <<- requires using Tab as indentation.
-	cat <<- EOF
-	black_rgb = im_black 1 1 3;
-	black_rgba = im_black 1 1 4;
+cat << EOF
+////////////////////////////////////////////////////////////
+// String utils.
 
-	// Getting width:
-	// im_header_int "width" img
-	// get_width img
+is_blank c
+	= true, c == ' '
+	= true, c == '\t'
+	= true, c == '\n'
+	= true, c == '\r'
+	= false;
+
+is_newline c
+	= true, c == '\n'
+	= true, c == '\r'
+	= false;
+
+is_tab c
+	= true, c == '\t'
+	= false;
+
+//lstrip s
+//	= lstrip (tl s), is_blank (s?0)
+//	= s;
+
+lstrip s
+	= dropwhile is_blank s;
+
+rstrip s
+	= reverse (lstrip (reverse s));
+
+strip s
+	= reverse (lstrip (reverse (lstrip s)));
+
+// Based on "split" from _list.def.
+// See split_newline for an example.
+split_once fn s
+	= [], s == []
+	= head : tail'
+{
+	head = takewhile (not @ fn) s;
+	tail = dropwhile (not @ fn) s;
+	tail'
+		= [], tail == []
+		= tl tail;
+}
+
+split_all fn s
+	= [], s == []
+	= head : split_all fn tail
+{
+	head : tail = split_once fn s;
+}
+
+// first : rest = split_newline "foo\nbar\nmore"
+// first -> "foo"
+// rest -> "bar\nmore"
+//
+// first : rest = split_newline "foo"
+// first -> "foo"
+// rest -> ""
+//
+// first : rest = split_newline "\nfoo"
+// first -> ""
+// rest -> "foo"
+split_newline s
+	= split_once is_newline s;
+
+// foo = split_newline "\nfoo\nbar\n\nmore\n"
+// foo = ["", "foo", "bar", "", "more"]
+split_newlines s
+	= split_all is_newline s;
+
+
+////////////////////////////////////////////////////////////
+// Alpha channel on images.
+
+// Alternative implementation of image_new,
+// if for some reason _generate.def is not available.
+alternative_image_new width height bands pixel
+	= out
+{
+	// 1x1 black pixel, uchar
+	black = im_black 1 1 bands;
+	// 1x1 black-or-gray-or-white pixel, float
+	white_float = black + pixel;
+	// 1x1 black-or-gray-or-white pixel, uchar
+	// white = im_clip2fmt white_float 0;
+	white = cast_unsigned_char white_float;
+	// im_embed <img> <type> <x_offset> <y_offset> <width> <height>
+	// type: 1 = extend  http://www.vips.ecs.soton.ac.uk/supported/current/doc/html/libvips/libvips-conversion.html#VipsExtend
+	out = im_embed white 1 0 0 width height;
+}
+
+// Adds an alpha channel (a 4th band) to a 3-band image.
+// If the image is 1-band, convert to a 4-band image.
+// Otherwise, leaves unchanged.
+// Only supports images with pixel range 0..255.
+// https://github.com/jcupitt/libvips/issues/325#issuecomment-136981134
+add_alpha img
+	= img, !is_image img
+	= img ++ img ++ img ++ alpha, (get_bands img) == 1
+	= img ++ alpha, (get_bands img) == 3
+	= img, (get_bands img) == 4
+	= img
+{
+	// image_new is defined at _generate.def.
+	// image_new <width> <height> <bands> <format> <coding> <type> <pixel> <x_offset> <y_offset>
+	// format: 0 = 8-bit unsigned int - UCHAR
+	// coding: 0 = none
+	// type: 0 = multiband; 1 = B_W
+	// pixel: 255 = fully opaque
+	//alpha = image_new (get_width img) (get_height img) 1 0 0 0 255 0 0;
+
+	alpha = alternative_image_new (get_width img) (get_height img) 1 255;
+}
+
+// Drops the 4th band, returning a 3-band image.
+remove_alpha img
+	= img, !is_image img
+	= im_extract_bands img 0 3, (get_bands img) == 4
+	= img;
+
+// Returns the 4th band of an image.
+get_alpha img
+	= im_extract_bands (add_alpha img) 3 1;
+
+// alpha_blend <img1> <img2> <x> <y> <alpha>
+// Puts <img2> on <img1> at <x>,<y>.
+// Respects RGBA alpha values from both images.
+// Preserves <img1> dimensions.
+// <alpha> is a multiplier for <img2> alpha channel.
+//         Useful values are between 0.0 and 1.0.
+//         It can be used to blend a opaque image in semi-transparent way.
+// Only supports RGB (3 bands) and RGBA (4 bands) images with pixels in range 0..255.
+alpha_blend base stamp x y alpha
+	= base, x >= (get_width base)
+	= base, y >= (get_height base)
+	= base, (x + (get_width stamp)) <= 0
+	= base, (y + (get_height stamp)) <= 0
+	= out
+{
+	// overlay is a transparent image with the same dimensions as the base,
+	// with the stamp placed at the desired position. If the stamp does not
+	// fit within the bounds, it will be cropped.
+	overlay = im_embed stamp 0 x y (get_width base) (get_height base);
 
 	// range is defined in _stdenv.def.
 	// range min value max = min_pair max (max_pair min value)
 
-	// Alternative implementation of image_new,
-	// if for some reason _generate.def is not available.
-	alternative_image_new width height bands pixel
-		= out
+	base_alpha    =  (cast_float (get_alpha base   )) / 255;
+	overlay_alpha = ((cast_float (get_alpha overlay)) / 255) * (range 0.0 alpha 1.0);
+
+	base_rgb    = (cast_float (remove_alpha base   )) / 255;
+	overlay_rgb = (cast_float (remove_alpha overlay)) / 255;
+
+	// https://en.wikipedia.org/wiki/Alpha_compositing#Alpha_blending
+	out_alpha = overlay_alpha + (base_alpha * (1 - overlay_alpha));
+	out_rgb = ((overlay_rgb * overlay_alpha) + (base_rgb * base_alpha * (1 - overlay_alpha))) / out_alpha;
+
+	out = (cast_unsigned_char (out_rgb * 255)) ++ (cast_unsigned_char (out_alpha * 255));
+}
+
+
+////////////////////////////////////////////////////////////
+// File-handling.
+
+read_file f
+	= out
+{
+	lines_from_file = split_newlines (read f);
+
+	split_fields line
+		= [(vips_image filename), (parse_int x), (parse_int y)]
 	{
-		// 1x1 black pixel, uchar
-		black = im_black 1 1 bands;
-		// 1x1 black-or-gray-or-white pixel, float
-		white_float = black + pixel;
-		// 1x1 black-or-gray-or-white pixel, uchar
-		// white = im_clip2fmt white_float 0;
-		white = cast_unsigned_char white_float;
-		// im_embed <img> <type> <x_offset> <y_offset> <width> <height>
-		// type: 1 = extend  http://www.vips.ecs.soton.ac.uk/supported/current/doc/html/libvips/libvips-conversion.html#VipsExtend
-		out = im_embed white 1 0 0 width height;
+		x : rest = split_once is_tab line;
+		y : filename = split_once is_tab rest;
 	}
 
-	// https://github.com/jcupitt/libvips/issues/325#issuecomment-136981134
-	add_alpha img
-		= img, !is_image img
-		= img, (get_bands img) == 4
-		= out
-	{
-		// image_new is defined at _generate.def.
-		// image_new <width> <height> <bands> <format> <coding> <type> <pixel> <x_offset> <y_offset>
-		// format: 0 = 8-bit unsigned int - UCHAR
-		// coding: 0 = none
-		// type: 0 = multiband; 1 = B_W
-		// pixel: 255 = fully opaque
-		//alpha = image_new (get_width img) (get_height img) 1 0 0 0 255 0 0;
+	out = [
+		(split_fields line) :: line <- lines_from_file
+	];
+}
 
-		alpha = alternative_image_new (get_width img) (get_height img) 1 255;
-		out = img ++ alpha;
-	}
+////////////////////////////////////////////////////////////
+// Misc.
 
-	remove_alpha img
-		= img, !is_image img
-		= img, (get_bands img) <= 3
-		= im_extract_bands img 0 3;
+black_rgb = im_black 1 1 3;
+black_rgba = im_black 1 1 4;
 
-	get_alpha img
-		= im_extract_bands (add_alpha img) 3 1;
+// Getting width:
+// im_header_int "width" img
+// get_width img
 
-	// alpha_blend <img1> <img2> <x> <y> <alpha>
-	// Puts <img2> on <img1> at <x>,<y>.
-	// Respects RGBA alpha values from both images.
-	// Preserves <img1> dimensions.
-	// <alpha> is a multiplier for <img2> alpha channel.
-	//         Useful values are between 0.0 and 1.0.
-	//         It can be used to blend a opaque image in semi-transparent way.
-	alpha_blend base stamp x y alpha
-		= base, x >= (get_width base)
-		= base, y >= (get_height base)
-		= base, (x + (get_width stamp)) <= 0
-		= base, (y + (get_height stamp)) <= 0
-		= out
-	{
-		// overlay is a transparent image with the same dimensions as the base,
-		// with the stamp placed at the desired position. If the stamp does not
-		// fit within the bounds, it will be cropped.
-		overlay = im_embed stamp 0 x y (get_width base) (get_height base);
+// Usage: join_at_xy <img> [ <img>, <x>, <y> ]
+// If the second <img> is just a zero, then return the first <img> unchanged.
+join_at_xy img meta
+	= img, !is_list meta
+	= im_insert img meta?0 meta?1 meta?2;
 
-		base_alpha    =  (cast_float (get_alpha base   )) / 255;
-		overlay_alpha = ((cast_float (get_alpha overlay)) / 255) * (range 0.0 alpha 1.0);
+// Same as join_at_xy, but with reversed parameter order.
+join_at_xy_r meta img
+	= join_at_xy img meta;
 
-		base_rgb    = (cast_float (remove_alpha base   )) / 255;
-		overlay_rgb = (cast_float (remove_alpha overlay)) / 255;
+// Choose one:
 
-		// https://en.wikipedia.org/wiki/Alpha_compositing#Alpha_blending
-		out_alpha = overlay_alpha + (base_alpha * (1 - overlay_alpha));
-		out_rgb = ((overlay_rgb * overlay_alpha) + (base_rgb * base_alpha * (1 - overlay_alpha))) / out_alpha;
+// main = large_rgb;
+// large_rgb  = foldl join_at_xy black_rgb  tiles_rgb;
 
-		out = (cast_unsigned_char (out_rgb * 255)) ++ (cast_unsigned_char (out_alpha * 255));
-	}
+// main = large_rgba;
+// large_rgba = foldl join_at_xy black_rgba tiles_rgba;
 
-	// Usage: join_at_xy <img> [ <img> <x> <y> ]
-	// If the second <img> is just a zero, then return the first <img> unchanged.
-	join_at_xy img meta
-		= img, !is_list meta
-		= im_insert img meta?0 meta?1 meta?2;
+main = large_rgb_r;
+large_rgb_r  = foldr join_at_xy_r black_rgb  tiles_rgb;
 
-	// Same as join_at_xy, but with reversed parameter order.
-	join_at_xy_r meta img
-		= join_at_xy img meta;
+// main = large_rgba_r;
+// large_rgba_r = foldr join_at_xy_r black_rgba tiles_rgba;
 
-	// Choose one:
-	//main = large_rgb;
-	//main = large_rgba;
-	main = large_rgb_r;
-	//main = large_rgba_r;
+tiles_rgb  = [ (remove_alpha h) : t :: h:t <- tiles];
+// tiles_rgba = [ (add_alpha    h) : t :: h:t <- tiles];
 
-	large_rgb  = foldl join_at_xy black_rgb  tiles_rgb;
-	large_rgba = foldl join_at_xy black_rgba tiles_rgba;
+tiles = read_file argv?1;
 
-	large_rgb_r  = foldr join_at_xy_r black_rgb  tiles_rgb;
-	large_rgba_r = foldr join_at_xy_r black_rgba tiles_rgba;
-
-	// hd is the list head, just the first element.
-	// tl is the list tail, the remaining of the list.
-	tiles_rgb  = [ [remove_alpha (hd t)] ++ tl t :: t <- tiles];
-	tiles_rgba = [ [add_alpha    (hd t)] ++ tl t :: t <- tiles];
-
-	tiles = init [
-	EOF
-
-	for (( row = 0 ; row < 9999 ; ++row )) ; do
-		first_tile=`tilename 0 ${row}`
-		[ -f "${first_tile}" ] || break
-
-		for (( col = 0 ; col < 9999 ; ++col )) ; do
-			next_tile=`tilename ${col} ${row}`
-			[ -f "${next_tile}" ] || break
-
-			echo "  [ (vips_image \"${next_tile}\") , $(( col * 256 )) , $(( row * 256 )) ],"
-		done
-	done
-	echo "  \"This element will be ignored and has been added just because the last element cannot be followed by a comma.\" "
-	echo "];"
+EOF
 }
 
 # For debugging purposes:
-nip2_script | cat -n
+#nip2_script | cat -n
+
+tmpfile="$(mktemp)" || {
+	echo "Failed to create a temporary file (using mktemp)."
+	exit 1
+}
+trap 'rm -f "${tmpfile}"' EXIT
+
+generate_file_list > "${tmpfile}"
 
 # https://github.com/jcupitt/nip2/issues/53
 #nip2_script | nip2 --verbose -d -b -o output.png
-nip2_script | nip2 --verbose -s /dev/stdin -o output.tif
+nip2_script | nip2 --verbose -s /dev/stdin -o output.png "${tmpfile}"
